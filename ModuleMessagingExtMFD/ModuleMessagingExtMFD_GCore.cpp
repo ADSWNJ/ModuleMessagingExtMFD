f4abbad6f01e864cb37085a08b82db4c37bbed2f
Andrew Stokes <andrew_d_stokes@hotmail.com>
2018-01-20 10:38:22 -0500

    Release version MMExt2 1.0

--- ModuleMessagingExtMFD/ModuleMessagingExtMFD_GCore.cpp
+++ ModuleMessagingExtMFD/ModuleMessagingExtMFD_GCore.cpp
@@ -35,11 +35,34 @@ void ModuleMessagingExtMFD_GCore::corePreStep(double simT,double simDT,double mj
     coreSimT = simT;
     int ix = 0;
     char typ;
-    string mod, var, vesName, msg;
+    string mod, var, vesName, msg, objdesc;
     OBJHANDLE ovh;
     list<string> mmListVesTyp, mmListModVar;
     while (mm.Find(&typ, &mod, &var, &ovh, &ix, "*", "*", NULL, false)) {
-      msg = string() + oapiGetVesselInterface(ovh)->GetName() + " (" + typ + ")";
+      if (typ == 'o') {
+        OBJHANDLE oh;
+        bool ret = mm.Get(mod, var, &oh); 
+        int ot = mm.ObjType(oh);
+        switch (ot) {
+        case -1:             objdesc = "o-err"; break;
+        case OBJTP_INVALID:  objdesc = "o-inv"; break;
+        case OBJTP_GENERIC:  objdesc = "o-gen"; break;
+        case OBJTP_CBODY:    objdesc = "o-cel"; break;
+        case OBJTP_STAR:     objdesc = "o-star"; break;
+        case OBJTP_PLANET:   objdesc = "o-planet"; break;
+        case OBJTP_VESSEL:   objdesc = "o-ves"; break;
+        case OBJTP_SURFBASE: objdesc = "o-base"; break;
+        }
+      } else {
+        objdesc = typ;
+      }
+      if (typ == 'x') {
+        objdesc = "struct";
+      }
+      if (typ == 'y') {
+        objdesc = "old struct";
+      }
+      msg = string() + oapiGetVesselInterface(ovh)->GetName() + " (" + objdesc + ")";
       mmListVesTyp.push_back(msg);
       mmListModVar.push_back(mod + ":" + var);
     }
